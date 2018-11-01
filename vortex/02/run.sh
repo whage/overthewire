d4d91ba5c38c722a3eace14875e9594b7c3e33bb
whage <whage@freemail.hu>
2018-11-01 10:59:55 +0100

    add notes

--- vortex/02/run.sh
+++ vortex/02/run.sh
@@ -1,5 +1,14 @@
-# none of the following seem to work, even when run directly
-# from the cli (and not executing this file)
+# None of the following seem to work, even when run directly
+# from the cli (and not executing this file).
+#
+# If I run these commands directly from an interactive bash
+# then the program starts, I get an sh shell (so far so good),
+# the first command runs and returns fine
+# but then the shell doesn't run any more commands,
+# pressing enter just adds new lines, ctrl+c doesn't kill it,
+# neither does sending EOF (ctrl+d).
+#
+# What's wrong?
 
 #(./solve.sh ; cat) | ./02
-#cat input_sequence.txt - | ./02
+cat input_sequence.txt - | ./02
