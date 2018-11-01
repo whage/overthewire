10c364efcd6a50a5a58b14d623cbf9997115dfa4
whage <whage@freemail.hu>
2018-11-01 10:27:39 +0100

    cleanup

--- vortex/02/brute_force.sh
+++
@@ -1,4 +0,0 @@
-while true; do
-    printf '\%.0s' {1..10}
-    printf 'a'
-done
--- vortex/02/test.c
+++
@@ -1,6 +0,0 @@
-int main() {
-    unsigned char buf[512];
-
-    printf("buffer size: %d\n", sizeof(buf));
-    printf("buffer value: %x\n", buf);
-}
