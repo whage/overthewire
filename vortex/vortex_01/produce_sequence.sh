f65b03963f91ec14a9fff3e11059d28144b4178d
András Sallai <ndrs.sallai@gmail.com>
2023-02-21 01:06:13 +0100

    rename folders

--- vortex/vortex_01/produce_sequence.sh
+++ vortex/vortex_01/produce_sequence.sh
@@ -1,7 +1,7 @@
 # We can step ptr backwards by sending a "\" character to stdin.
 # First it points to the middle of the buffe: buf + 256.
 # The pointer itself is at buf - 8 (8 byte, little-endian pointers on my machine).
-# We must make the 4th byte of the pinter equal to 0xCA.
+# We must make the 4th byte of the pointer equal to 0xCA.
 # So we must send 256 + 5 backslashes and then the value 0xCA followed by any
 # character to trigger the branch of the case statement that calls e();
 
